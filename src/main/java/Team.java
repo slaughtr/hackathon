ea1b39a50774986cb6e85f753a22c1592b15dcc0
Dallas Slaughter <dslaughtr@gmail.com>
2017-03-24 08:32:18 -0700

    Add basic logic to class, pass first basic tests

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -1,3 +1,20 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Team {
+  private String mName;
+  public static ArrayList<Team> teamsArray = new ArrayList<Team>();
+
+  public Team(String name) {
+    mName = name;
+  }
+
+  public String getTeamName() {
+    return mName;
+  }
+
+  public static void clearTeamArray() {
+    teamsArray.clear();
+  }
 
 }
--- src/test/java/TeamTest.java
+++ src/test/java/TeamTest.java
@@ -3,4 +3,20 @@ import static org.junit.Assert.*;
 
 public class TeamTest {
 
+  @After
+  public void tearDown() {
+    Team.clearTeamArray();
+  }
+
+  @Test
+  public void team_InstantiatesCorrectly_true() {
+    Team testTeam = new Team("test");
+    assertTrue(testTeam instanceof Team);
+  }
+
+  @Test
+  public void team_SetsNameCorrectly_true() {
+    Team testTeam = new Team("test");
+    assertTrue(testTeam.getTeamName().equals("test"));
+  }
 }
