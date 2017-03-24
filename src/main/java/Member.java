41e234a41f1322c3a0e17544502c6c44517d199a
Dallas Slaughter <dslaughtr@gmail.com>
2017-03-24 08:44:51 -0700

    Add basic name, id, array to Member class, basic tests passing

--- src/main/java/Member.java
+++ src/main/java/Member.java
@@ -1,3 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Member {
+  private String mName;
+  private static ArrayList<Member> memberArray = new ArrayList<Member>();
+  private int mId;
+
+  public Member(String name) {
+    mName = name;
+    memberArray.add(this);
+  }
+
+  public String getMemberName() {
+    return mName;
+  }
+
+
+  public static void clearMemberArray() {
+    memberArray.clear();
+  }
+
+  public static List<Member> getAllMembers() {
+    return memberArray;
+  }
 
 }
--- src/test/java/MemberTest.java
+++ src/test/java/MemberTest.java
@@ -3,4 +3,21 @@ import static org.junit.Assert.*;
 
 public class MemberTest {
 
+  @After
+  public void tearDown() {
+    Member.clearMemberArray();
+  }
+
+  @Test
+  public void member_instantiatesCorrectly_true() {
+    Member testMember = new Member("test");
+    assertTrue(testMember instanceof Member);
+  }
+
+  @Test
+  public void member_setsNameCorrectly_true() {
+    Member testMember = new Member("test");
+    assertTrue(testMember.getMemberName().equals("test"));
+  }
+
 }
